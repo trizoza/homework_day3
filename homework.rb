342ccbe1e3fc316c8c0b4da7e0ccde604dc047e7
Peter Trizuliak <peter.trizuliak@gmail.com>
2016-11-23 17:10:06 +0000

    Homework finished

--- homework.rb
+++ homework.rb
@@ -73,20 +73,39 @@ users = {
   }
 }
 
-# 1. Return Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+# # 1. Return Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+# puts users["Jonathan"][:twitter]
+# # 2. Return Erik's hometown
+# puts users["Erik"][:home_town]
+# # 3. Return the array of Erik's favorite numbers
+# puts users["Erik"][:favourite_numbers]
+# # 4. Return the type of Avril's pet Colin
+# puts users["Avril"][:pets]["colin"]
+# # 5. Return the smallest of Erik's favorite numbers
+# puts users["Erik"][:favourite_numbers].min
+# # 6. Add the number `7` to Erik's favorite numbers
+# users["Erik"][:favourite_numbers] << 7
+# puts users["Erik"][:favourite_numbers]
+# # 7. Change Erik's hometown to Edinburgh
+# users["Erik"][:home_town] = "Edinburgh"
+# puts users["Erik"][:home_town]
+# # 8. Add a pet dog to Erik called "Fluffy"
+# users["Erik"][:pets].store("Fluffy", :dog)
+# puts users["Erik"][:pets]
+# # 9. Add yourself to the users hash
+# users.store(
+#   "Peter", {
+#     :twitter => "non_existant",
+#     :favourite_numbers => [3],
+#     :home_town => "Zilina",
+#     :pets => {
+#       "Snoop" => :dogg,
+#       "Dory" => :fish,
+#       "KingKong" => :monkey
+#     }
+#   }
+# )
 
-# 2. Return Erik's hometown
+#users["Dave"] = {"the rest"}
 
-# 3. Return the array of Erik's favorite numbers
-
-# 4. Return the type of Avril's pet Colin
-
-# 5. Return the smallest of Erik's favorite numbers
-
-# 6. Add the number `7` to Erik's favorite numbers
-
-# 7. Change Erik's hometown to Edinburgh
-
-# 8. Add a pet dog to Erik called "Fluffy"
-
-# 9. Add yourself to the users hash
+#puts users["Peter"]
